be1291fb89c905c6d2260d89fdaf070751918d04
MasterAli2 <88894545+MasterAli2@users.noreply.github.com>
2025-09-08 18:56:08 -0400

    added debug option

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 from functools import wraps
 from flask import request
 
+import config
+
 def discord_only(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if "Discordbot" not in request.headers.get("User-Agent", ""):
+        if "Discordbot" not in request.headers.get("User-Agent", "") and not config.DEBUG:
             return "This endpoint is only accessible through Discord", 403
         return func(*args, **kwargs)
     return wrapper
